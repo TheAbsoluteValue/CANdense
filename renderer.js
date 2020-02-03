b2bd55527a86cb10e0a660e2a171078676357fb7
kaiser <soccerbeastwc@gmail.com>
2020-02-03 13:38:53 -0500

    Quick renderer fix and message format

--- renderer.js
+++ renderer.js
@@ -2,7 +2,9 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const tableify = require('tableify');
 
-const port = new SerialPort('/dev/ttyUSB1', {
+const messages = {};
+
+const port = new SerialPort('/dev/ttyUSB0', {
     baudRate: 115200,
     parser: SerialPort.parsers.readline
 });
@@ -12,5 +14,8 @@ port.on('open', () => {
 });
 
 port.on('data', data => {
-    console.log(data.toString());
+    // console.log(data.toString());
+    const dataSplit = data.toString().split(' ');
+    messages[dataSplit[0]] = {id: dataSplit[0], data: dataSplit.slice(1, dataSplit.length - 1)};
+    console.log(messages);
 });
