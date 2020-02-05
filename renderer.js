aceb5ff41cf18cc2a33085d6cde0ef4369b840cb
Blake <ama542blake@gmail.com>
2020-02-04 21:50:58 -0500

    Got a mock binding working

--- renderer.js
+++ renderer.js
@@ -3,35 +3,36 @@ const MockBinding = require('@serialport/binding-mock');
 const Readline = require('@serialport/parser-readline');
 const tableify = require('tableify');
 
-// create a mock binding
 SerialPort.Binding = MockBinding;
-MockBinding.createPort('/dev/Mock');
-portIsOpen = false;
-/* writing to the port of the mock binding requires a buffer
-  Buffer documentation: https://nodejs.org/api/buffer.html#buffer_buffer
-  About writing to ports: https://serialport.io/docs/api-binding-abstract
-*/
-mockMessage = Buffer.from('420 69 420');
 
 const messages = {};
 
+// if echo is false, then port.on('data', ...) won't fire
+MockBinding.createPort('PORT_PATH', {echo: true, record: false});
+
 //const port = new SerialPort('/dev/ttyUSB0', { // actual port
-const port = new SerialPort('/dev/Mock', { // for testing
+const port = new SerialPort('PORT_PATH', { // for testing
     baudRate: 115200,
     parser: SerialPort.parsers.readline
 });
 
 port.on('open', () => {
     console.log('port opened');
-    port.write(mockMessage);
 });
+
+// write message to buffer
+mockMessage = Buffer.from('420 69 69420');
+port.write(mockMessage)//, () => {
+  //console.log('Message written successfully!');
+  //console.log('Last write: ', port.binding.lastWrite.toString('utf8'));
+//})
 
 port.on('data', data => {
-    console.log(data.toString());
-    // const dataSplit = data.toString().split(' ');
-    // messages[dataSplit[0]] = {id: dataSplit[0], data: dataSplit.slice(1, dataSplit.length - 1)};
-    // console.log(messages);
+    console.log('Received some data:\n\t', data.toString());
+    const dataSplit = data.toString().split(' ');
+    messages[dataSplit[0]] = {id: dataSplit[0], data: dataSplit.slice(1, dataSplit.length - 1)};
+    console.log(messages);
 });
 
-port.binding.emitData(mockMessage); // why isn't the port open by this point?
-//port.binding.write(mockMessage); // same here
+// have the port pretend like it received data (without actually calling port, write, I think)
+//port.binding.emitData(mockMessage);
