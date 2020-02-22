48c19141e609dc69ce5a3475e826f90d64b0d877
Ryan Sadowski <rsadowski@oakland.edu>
2020-02-22 13:07:04 -0500

    read data from JSON, convert to table

--- filereader.js
+++ filereader.js
@@ -5,10 +5,13 @@ const tableify = require('tableify');
 try {
     if (fs.existsSync(path)) {
         var data = JSON.parse(fs.readFileSync(path));
-        var html = tableify(data);
-        var table = document.createElement("tbody");
-        table.appendChild(html);
-        console.log(html);
+        var title = data['title'];
+        var note = data['note'];
+        var messages = data['data'];
+        var html = tableify(messages);
+        document.getElementById("carMake").innerHTML = title ? title: 'CanDense' ;
+        document.getElementById("notes").innerHTML = note ? note: 'notes';
+        document.getElementById("table").innerHTML = html;
     } else { 
         console.log('file not found!');
     }   
