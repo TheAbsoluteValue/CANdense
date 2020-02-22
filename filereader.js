3a53c40d95fbc5f62a32238d330e748c27be75ec
Ryan Sadowski <rsadowski@oakland.edu>
2020-02-22 12:18:11 -0500

    starting a table

--- filereader.js
+++ filereader.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = './data.json';
+const tableify = require('tableify');
 
 try {
     if (fs.existsSync(path)) {
-        let data = fs.readFileSync(path);
-        let parsedData = JSON.parse(data);
-        console.log(parsedData);
+        var data = JSON.parse(fs.readFileSync(path));
+        var html = tableify(data);
+        var table = document.createElement("tbody");
+        table.appendChild(html);
+        console.log(html);
     } else { 
         console.log('file not found!');
     }   
