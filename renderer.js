0d84d810d8b42f471bcf802afee503ed437bb296
Blake <ama542blake@gmail.com>
2020-03-01 21:01:42 -0500

    Update renderer from Blake branch

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const SerialPort = require('serialport');
 const MockBinding = require('@serialport/binding-mock');
 const Readline = require('@serialport/parser-readline');
 const fs = require('fs');
-//const stream = require('stream');
+const tableify = require('tableify');
 
 const messages = {};
 
@@ -16,31 +16,76 @@ const port = new SerialPort('PORT_PATH', { // for testing
     parser: SerialPort.parsers.readline
 });
 
-console.log(port.on('open', () => {
-    console.log('Incoming data port opened.');
-}));
+var dataJson = {
+	"_id": "1",
+	"title": "2010 Honda Accord",
+	"data": [{
+		"ID": "1",
+		"message": "some data",
+		"time": "UTC",
+		"count": 1
+	}],
+	"label": "",
+	"note": ""
+};
 
-c = 0;
-port.on('data', data => {
-  lineSplit = data.toString().split('\n');
-  lineSplit.forEach(item => {
-    //const dataSplit = data.toString().split(' ');
-    c++;
-    if (c <= 1) {
-      console.log(`SPLIT ${c}: ${item.toString()}`);
-    }
-  });
-});
+/*
+  How data is read in and parsed:
+  ReadStream (only when reading the log file) => port => parser
+  We use the ReadStream to read from the log file. This won't be done when reading live from
+  a car. The data that comes in to the port, whether from a log file or directly from the CAN
+  bus is place in to a buffer. At times, a message will overflow in to the next buffer. By piping
+  the port in to the ReadLine parser, we eliminate this problem because it only emits data once a
+  newline is encountered– not when the buffer is full.
+ */
+ // creates the ReadLine parser, which is the final destination for the data
+ const parser = port.pipe(new Readline());
+ let counter = 0;
+ parser.on('data', data => {
+   let dataSplit = data.toString().split(' ');
+   // get the ID and message data
+   let id = dataSplit[2].slice(0, 3);
+   let messageData = dataSplit[2].slice(4);
 
-logFile = fs.createReadStream('test_CANdump1_abbreviated.log');
-logFile.on('open', () => {
-  console.log('CAN dump log opened.\n\n\n\n');
-});
+   // make the time stamp human-readable
+   let unixTimeStamp = dataSplit[0].slice(1, -1);
+   let date = new Date(parseFloat(unixTimeStamp));
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   let seconds = date.getSeconds();
+   let milliseconds = date.getMilliseconds();
+   let arr = [hours, minutes, seconds, milliseconds];
+   let timeString = [hours, minutes, seconds, milliseconds].join(':');
 
-logFile.on('ready', () => {
-  console.log('CAN dump reader is ready.');
-  logFile.pipe(port);
-});
+   dataJson.data.push({"ID": id,
+                       "message": messageData,
+                       "time": timeString,
+                       "count": 1});
+   var title = dataJson['title'];
+   var note = dataJson['note'];
+   var message = dataJson['data'];
+
+   // create table from JSON data array
+   var html = tableify(message);
+
+   // write to DOM
+   document.getElementById("carMake").innerHTML = title ? title : 'CanDense';
+   document.getElementById("notes").innerHTML = note ? note : 'notes';
+   document.getElementById("table").innerHTML = html;
+ });
+
+
+ // once the serial port is open, we add a pipe in to the parser
+ port.on('open', () => {
+   port.pipe(parser);
+ });
+
+
+ // create the ReadStream (only useful when reading from a log file)
+ logFile = fs.createReadStream('test_CANdump1_abbreviated.log');
+ logFile.on('open', () => {
+   logFile.pipe(port);  // send all data to the port
+ });
 
 sum = 0;
 logFile.on('data', data => {
