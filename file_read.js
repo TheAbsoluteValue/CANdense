fa1175c4ad3aaadb3415807ae431e6390460d568
Blake <ama542blake@gmail.com>
2020-03-03 23:23:12 -0500

    More progress
    Having trouble with specifying the path to the log file. A file can't be found there for some reason

--- file_read.js
+++ file_read.js
@@ -3,13 +3,74 @@ const path = './data.json';
 const rendererPath = './renderer.js';
 const fs = require('fs');
 
-let logFileName;
+// creates a file object for the file the user chose from the file input
+// once the file is set, it calls the function that will actually read it
+document.getElementById('set-path-btn').addEventListener('click', () => {
+	let filePath = document.getElementById('logfile-path').value;
+	if (!filePath) {
+		alert("Please enter a file path");
+	} else {
+		readLogFile(filePath);
+	}
+});
 
-function setLogfile() {
-	// get the name of the selected from filel
-	logFileName = document.getElementById("logfile-selector").value.slice(12);
-	alert(logFileName);
+function readLogFile(path) {
+	try {
+		fs.readFile(path, 'r', 'utf-8', (err, data) => {
+			if(data){
+					// split the log file on each line, then go through and push necessary elements to JSON
+					lineSplit = data.toString().split('\n');
+					for(var i = 0; i <= lineSplit.length - 1; i++) {
+							if(lineSplit[i] !== '') {
+							var timestamp = lineSplit[i].substring(0, 19);
+							var id = lineSplit[i].substring(25, 28);
+							var info = lineSplit[i].substring(29, lineSplit[i].length);
+							dataJson.data.push({"ID":id,"Message":info,"Time":timestamp,"Count":1});
+							}
+					}
+
+					// sort by ID, store in temp JSON obj
+					var sortedDataJson = sortById(dataJson.data);
+
+					// get number of messages for each ID, will be seperate table
+					var occurences = {};
+					// reduce the sorted data JSON to an object of key-value -> id: count
+					occurences = countMessagesById(sortedDataJson);
+					// convert the object to an array
+					occurencesArr = Object.entries(occurences);
+					// sort the array, because for some reason it's unsorted now
+					occurencesArr = sortOccurencesArray(occurencesArr);
+					// give headers to array - for UI table purposes
+					occurencesArr.unshift(["ID", "Count"]);
+
+					// store title, note, data for use with DOM
+					var title = dataJson['title'];
+					var note = dataJson['note'];
+					var message = sortedDataJson;
+
+					// if you want unsorted, use line below instead of line above
+					// var message = dataJson['data'];
+
+					// create tables from JSON data array
+					var occurencesHtml = tableify(occurencesArr);
+					var html = tableify(message);
+
+					// write to DOM
+					document.getElementById("carMake").innerHTML = title ? title : 'CanDense';
+					document.getElementById("notes").innerHTML = note ? note : 'notes';
+					document.getElementById("occurenceTable").innerHTML = occurencesHtml;
+					document.getElementById("table").innerHTML = html;
+
+					// make sure preceding is done within callback
+			} else {
+				alert('The file is empty');
+			}
+		});
+	} catch (e) {
+		alert(`${path} does not exist`);
+	}
 }
+
 
 
 
@@ -55,57 +116,57 @@ function setLogfile() {
 //             if(err) {
 //                 console.error(err);
 //             }
-//             if(data){
-//                 // split the log file on each line, then go through and push necessary elements to JSON
-//                 lineSplit = data.toString().split('\n');
-//                 for(var i = 0; i <= lineSplit.length - 1; i++) {
-//                     if(lineSplit[i] !== '') {
-//                     var timestamp = lineSplit[i].substring(0, 19);
-//                     var id = lineSplit[i].substring(25, 28);
-//                     var info = lineSplit[i].substring(29, lineSplit[i].length);
-//                     dataJson.data.push({"ID":id,"Message":info,"Time":timestamp,"Count":1});
-//                     }
-//                 }
-//                 // console.log(dataJson);
-//
-//                 // sort by ID, store in temp JSON obj
-//                 var sortedDataJson = sortById(dataJson.data);
-//
-//                 // get number of messages for each ID, will be seperate table
-//                 var occurences = {};
-//                 // reduce the sorted data JSON to an object of key-value -> id: count
-//                 occurences = countMessagesById(sortedDataJson);
-//                 // convert the object to an array
-//                 occurencesArr = Object.entries(occurences);
-//                 // sort the array, because for some reason it's unsorted now
-//                 occurencesArr = sortOccurencesArray(occurencesArr);
-//                 // give headers to array - for UI table purposes
-//                 occurencesArr.unshift(["ID", "Count"]);
-//
-//                 // store title, note, data for use with DOM
-//                 var title = dataJson['title'];
-//                 var note = dataJson['note'];
-//                 var message = sortedDataJson;
-//
-//                 // if you want unsorted, use line below instead of line above
-//                 // var message = dataJson['data'];
-//
-//                 // create tables from JSON data array
-//                 var occurencesHtml = tableify(occurencesArr);
-//                 var html = tableify(message);
-//
-//                 // write to DOM
-//                 document.getElementById("carMake").innerHTML = title ? title : 'CanDense';
-//                 document.getElementById("notes").innerHTML = note ? note : 'notes';
-//                 document.getElementById("occurenceTable").innerHTML = occurencesHtml;
-//                 document.getElementById("table").innerHTML = html;
-//
-//                 // make sure preceding is done within callback
-//             }
-//         });
-//     } else {
-//         console.log('file not found!');
-//     }
+            // if(data){
+            //     // split the log file on each line, then go through and push necessary elements to JSON
+            //     lineSplit = data.toString().split('\n');
+            //     for(var i = 0; i <= lineSplit.length - 1; i++) {
+            //         if(lineSplit[i] !== '') {
+            //         var timestamp = lineSplit[i].substring(0, 19);
+            //         var id = lineSplit[i].substring(25, 28);
+            //         var info = lineSplit[i].substring(29, lineSplit[i].length);
+            //         dataJson.data.push({"ID":id,"Message":info,"Time":timestamp,"Count":1});
+            //         }
+            //     }
+            //     // console.log(dataJson);
+						//
+            //     // sort by ID, store in temp JSON obj
+            //     var sortedDataJson = sortById(dataJson.data);
+						//
+            //     // get number of messages for each ID, will be seperate table
+            //     var occurences = {};
+            //     // reduce the sorted data JSON to an object of key-value -> id: count
+            //     occurences = countMessagesById(sortedDataJson);
+            //     // convert the object to an array
+            //     occurencesArr = Object.entries(occurences);
+            //     // sort the array, because for some reason it's unsorted now
+            //     occurencesArr = sortOccurencesArray(occurencesArr);
+            //     // give headers to array - for UI table purposes
+            //     occurencesArr.unshift(["ID", "Count"]);
+						//
+            //     // store title, note, data for use with DOM
+            //     var title = dataJson['title'];
+            //     var note = dataJson['note'];
+            //     var message = sortedDataJson;
+						//
+            //     // if you want unsorted, use line below instead of line above
+            //     // var message = dataJson['data'];
+						//
+            //     // create tables from JSON data array
+            //     var occurencesHtml = tableify(occurencesArr);
+            //     var html = tableify(message);
+						//
+            //     // write to DOM
+            //     document.getElementById("carMake").innerHTML = title ? title : 'CanDense';
+            //     document.getElementById("notes").innerHTML = note ? note : 'notes';
+            //     document.getElementById("occurenceTable").innerHTML = occurencesHtml;
+            //     document.getElementById("table").innerHTML = html;
+						//
+            //     // make sure preceding is done within callback
+            // }
+    //     });
+    // } else {
+    //     console.log('file not found!');
+    // }
 // } catch (err) {
 //     console.error(err);
 // }
