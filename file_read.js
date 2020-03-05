41e70bbd74b481f2eda41bdf0d3e856051833e5a
Ryan Sadowski <rsadowski@oakland.edu>
2020-03-04 21:09:44 -0500

    file selector for file reader

--- file_read.js
+++ file_read.js
@@ -2,14 +2,37 @@ const tableify = require('tableify');
 const path = './data.json';
 const fs = require('fs');
 
-document.getElementById('read-btn').addEventListener('click', () => {
-	let filePath = document.getElementById('logfile-path').value;
-	if (!filePath) {
-		alert("Please enter a file path");
-	} else {
-		readLogFile(filePath);
-	}
-});
+var selectedPath = '';
+var dataJson = {
+	"_id": "1",
+	"title": "2010 Honda Accord",
+	"data": [{
+        "ID": "1",
+		"Message": "some data",
+		"Time": "UTC",
+		"Count": 1
+    }],
+    "Count": 1,
+	"label": "",
+	"note": ""
+};
+
+run();
+
+function run() {
+	populateSelectFileDropdown();
+	registerFileBtn();
+}
+
+function registerFileBtn() {
+	document.getElementById('read-btn').addEventListener('click', () => {
+		if (!selectedPath) {
+			alert("Please enter a file path");
+		} else {
+			readLogFile(selectedPath);
+		}
+	});
+}
 
 function readLogFile(path) {
 	try {
@@ -68,87 +91,27 @@ function readLogFile(path) {
 	}
 }
 
-var dataJson = {
-	"_id": "1",
-	"title": "2010 Honda Accord",
-	"data": [{
-        "ID": "1",
-		"Message": "some data",
-		"Time": "UTC",
-		"Count": 1
-    }],
-    "Count": 1,
-	"label": "",
-	"note": ""
-};
+function populateSelectFileDropdown() {
+    // get select ID, for file selection in current directory
+    let filePath = document.getElementById('logfile-path');
+    // populate var with current files in directory
+    var fileOptions = fs.readdirSync('./');
+    // filter above to only include log files
+    var logFiles = fileOptions.filter(file => file.endsWith('.log'));
+    // populate select options
+    logFiles.forEach(option => filePath.options.add(new Option(option)));
+}
 
-//
-// try {
-//     // if file exists
-//     if (fs.existsSync(logPath)) {
-//         // parse JSON file - no longer needed but could be useful
-//         // var data = JSON.parse(fs.readFileSync(path));
-//
-//         // parse Log file
-//         var data = fs.readFile(logPath, 'utf-8', (err, data) => {
-//             if(err) {
-//                 console.error(err);
-//             }
-            // if(data){
-            //     // split the log file on each line, then go through and push necessary elements to JSON
-            //     lineSplit = data.toString().split('\n');
-            //     for(var i = 0; i <= lineSplit.length - 1; i++) {
-            //         if(lineSplit[i] !== '') {
-            //         var timestamp = lineSplit[i].substring(0, 19);
-            //         var id = lineSplit[i].substring(25, 28);
-            //         var info = lineSplit[i].substring(29, lineSplit[i].length);
-            //         dataJson.data.push({"ID":id,"Message":info,"Time":timestamp,"Count":1});
-            //         }
-            //     }
-            //     // console.log(dataJson);
-						//
-            //     // sort by ID, store in temp JSON obj
-            //     var sortedDataJson = sortById(dataJson.data);
-						//
-            //     // get number of messages for each ID, will be seperate table
-            //     var occurences = {};
-            //     // reduce the sorted data JSON to an object of key-value -> id: count
-            //     occurences = countMessagesById(sortedDataJson);
-            //     // convert the object to an array
-            //     occurencesArr = Object.entries(occurences);
-            //     // sort the array, because for some reason it's unsorted now
-            //     occurencesArr = sortOccurencesArray(occurencesArr);
-            //     // give headers to array - for UI table purposes
-            //     occurencesArr.unshift(["ID", "Count"]);
-						//
-            //     // store title, note, data for use with DOM
-            //     var title = dataJson['title'];
-            //     var note = dataJson['note'];
-            //     var message = sortedDataJson;
-						//
-            //     // if you want unsorted, use line below instead of line above
-            //     // var message = dataJson['data'];
-						//
-            //     // create tables from JSON data array
-            //     var occurencesHtml = tableify(occurencesArr);
-            //     var html = tableify(message);
-						//
-            //     // write to DOM
-            //     document.getElementById("carMake").innerHTML = title ? title : 'CanDense';
-            //     document.getElementById("notes").innerHTML = note ? note : 'notes';
-            //     document.getElementById("occurenceTable").innerHTML = occurencesHtml;
-            //     document.getElementById("table").innerHTML = html;
-						//
-            //     // make sure preceding is done within callback
-            // }
-    //     });
-    // } else {
-    //     console.log('file not found!');
-    // }
-// } catch (err) {
-//     console.error(err);
-// }
-//
+// on file change, return a string of the correct path depending on OS
+function selectionChanged(event) {
+	var os = getOS();
+	if (os === "Windows") {
+		selectedPath = './' + event.target.value;
+	} else {
+		selectedPath = '.\\' + event.target.value;
+	}
+}
+
 // sort by ID, then combine like messages within same ID
 function sortById(data) {
     return data.sort((a, b) => (a.ID > b.ID) ? 1 : -1);
@@ -166,4 +129,15 @@ function countMessagesById(data) {
 // needed this for sorting array of message occurences per ID
 function sortOccurencesArray(arr) {
     return arr.sort((a, b) => (a[0] > b[0]) ? 1 : -1);
+}
+
+// use navigator to figure out which OS you are on, useful for file directory stuff
+function getOS() {
+	if (navigator.appVersion.indexOf("Win") != -1) {
+		return "Windows";
+	} else if (navigator.appVersion.indexOf("Mac") != -1) {
+		return "MacOS"
+	} else {
+		return "Linux";
+	}
 }
