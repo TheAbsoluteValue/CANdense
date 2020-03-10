6145cd19c64ce0eaec81a39a58f65d62c2bff200
Blake <ama542blake@gmail.com>
2020-03-10 13:49:16 -0400

    Started adding trasmitting capability
    Still needs to validate that user input is valid

--- transmit.js
+++ transmit.js
@@ -1,1 +1,59 @@
+const SerialPort = require('serialport');
+const MockBinding = require('@serialport/binding-mock');
 
+// set up the port to which we write to
+SerialPort.Binding = MockBinding;
+MockBinding.createPort('PORT_PATH', {
+  echo: true,
+  record: false
+});
+const port = new SerialPort('PORT_PATH', {
+  baudRate: 115200,
+  highWaterMark: 90 // max buffer size of the port
+});
+
+// returns true if the input is a valid hexadecimal number
+function isValidHex(string) {
+  // TODO: figure out the right regular expression and finish this
+  let re = /[a-f]|[A-F]|[0-9]/;
+}
+
+// write to port when button is pushed
+document.getElementById('transmit-btn').addEventListener('click', () => {
+  // the ID for the message that the user wants to transmit (required)
+  let id = document.getElementById('transmit-id').value;
+  // the data field for the message that the user wants to transmit (required)
+  let data = document.getElementById('transmit-data').value;
+  // the interval at which the user wants to transmit the message (in ms) (optional, defaults to 1000ms)
+  let interval = document.getElementById('transmit-interval').value;
+  if (!interval || interval < 1) {interval = 1000}
+  // the number of times the user wants to transmit the message (defaults to 1)
+  let count = document.getElementById('transmit-count').value;
+  if (!count || count < 1) {count = 1}
+  let transmissionString = `${id}#${data}`;
+
+  /*
+  The normal setInterval() function also adds the delay before the first call.
+  This function first calls the function immediately (to avoid the initial delay),
+  then sets the interval at the specified time.
+  */
+  function setIntervalAndCallImmediately(f, interval) {
+    if (id && data) {
+      f();  // the initial call (to avoid the initial delay)
+      count--;
+      timeoutID = setInterval(() => {
+        if (count > 0) {
+          f();
+          count--;
+        } else {
+          clearInterval(timeoutID); // stop the interval calls
+        }
+      }, interval);
+    }
+  }
+
+  setIntervalAndCallImmediately(() => {
+    console.log(`Writing to port: ${transmissionString}`);
+    port.write(transmissionString);
+  }, interval);
+});
