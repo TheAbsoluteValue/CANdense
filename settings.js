93159ccc1271f43946619fb71f710bd1cad1e03a
Blake <ama542blake@gmail.com>
2020-03-10 17:03:56 -0400

    More work on allowing user to add vehicle profile

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 // When the user selects a new vehicle, this event fires
 function selectionChanged(event) {
-  newVehicleName = event.target.value;
+  let newVehicleName = event.target.value;
 }
 
 // prepares for user to select item from drop down
 function run() {
-	os = getOS();
+	let os = getOS();
 	populateSelectFileDropdown();
 	registerFileBtn();
 }
@@ -34,11 +34,34 @@ function populateSelectFileDropdown() {
 		vehiclesJSON = JSON.parse(fs.readFileSync(process.cwd() + '/vehicles.config'));
 	}
     // keys of vehiclesJSON is the name of the vehicle
-    var vehicleNames = vehiclesJSON.keys();
+    let vehicleNames = vehiclesJSON.keys();
     // populate select options
     vehicleNames.forEach(vehicleName => filePath.options.add(new Option(vehicleName)));
 }
 
 document.getElementById('select-vehicle-btn').addEventListener('click', () => {
 
+});
+
+/*
+Allows user to add a vehicle to the list of vehicles. Also adds that vehicle to the
+vehicles.config JSON file.
+TODO (maybe): Just put all the new inputs and buttons in the HTML from the beginning,
+but just hide them and then unhide them when the user clicks the + button.
+TODO: Hide the input and button again after the user adds the vehicle.
+*/
+let addVehicleBtn = document.getElementById('add-vehicle-btn');
+addVehicleBtn.addEventListener('click', () => {
+  // create the DOM objects
+  let newVehicleInput = document.createElement("input");
+  newVehicleInput.setAttribute("id", "new-vehicle-input");
+  let newVehicleBtn = document.createElement("button");
+  newVehicleBtn.innerHTML = "Save vehicle";
+  newVehicleBtn.setAttribute("id", "new-vehicle-btn");
+
+  // add the new object to the DOM
+  addVehicleBtn.insertAdjacentElement('afterend', newVehicleInput);
+  newVehicleInput.insertAdjacentElement("beforebegin", document.createElement('br'));
+  newVehicleInput.insertAdjacentElement('afterend', newVehicleBtn);
+  newVehicleInput.insertAdjacentElement('afterend', document.createElement('br'));
 });
